3f2c44ea7479e7bb27da15c5402d207a05e99d94
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-08 12:01:33 -0700

    Comments added.

--- 3.1 Express Server/index.js
+++ 3.1 Express Server/index.js
@@ -1,7 +1,13 @@
+// Using latest version module to express
 import express from "express";
+
+// Getting a hold of Express
 const app = express();
+
+// Step 1. Port is the location where we are listening.
 const port = 3000;
 
+// Request, Response
 app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
+  console.log(`Server running on port ${port}.`); // <- Step 2. Callback
 });
